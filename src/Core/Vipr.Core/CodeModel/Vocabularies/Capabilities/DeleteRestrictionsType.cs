e850625534c84cd9f185c57c7d0c5746c720eb81
Tom White <twhite@microsoft.com>
2015-02-06 17:15:35 -0800

    Add representations of ComplexTypes in Capabilities vocab
    There are a few different complex types that are used in the
    OneNote metadata document. This commit adds those complex types so that
    they may be parsed and represented.
    
    This is obviously incomplete coverage of the annotations. Additionally,
    this code should not be hand written and probably should be generated.

---
+++ src/Core/Vipr.Core/CodeModel/Vocabularies/Capabilities/DeleteRestrictionsType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vipr.Core.CodeModel.Vocabularies.Capabilities
+{
+    /*      <Term Name="DeleteRestrictions" Type="Capabilities.DeleteRestrictionsType" AppliesTo="EntitySet">
+        <Annotation Term="Core.Description" String="Restrictions on delete operations" />
+      </Term>
+      <ComplexType Name="DeleteRestrictionsType">
+        <Property Name="Deletable" Type="Edm.Boolean" DefaultValue="true">
+          <Annotation Term="Core.Description" String="Entities can be deleted" />
+        </Property>
+        <Property Name="NonDeletableNavigationProperties" Type="Collection(Edm.NavigationPropertyPath)">
+          <Annotation Term="Core.Description" String="These navigation properties do not allow DeleteLink requests" />
+        </Property>
+      </ComplexType>
+*/
+    /// <summary>
+    /// Restrictions on delete operations
+    /// </summary>
+    public class DeleteRestrictionsType
+    {
+        /// <summary>
+        /// Entities can be deleted
+        /// </summary>
+        public bool Deletable { get; set; }
+
+        /// <summary>
+        /// These navigation properties do not allow DeleteLink requests
+        /// </summary>
+        public List<string> NonDeletableNavigationProperties { get; set; }
+
+        public DeleteRestrictionsType()
+        {
+            Deletable = true;
+            NonDeletableNavigationProperties = new List<string>();
+        }
+    }
+}
---
+++ src/Core/Vipr.Core/CodeModel/Vocabularies/Capabilities/ExpandRestrictionsType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vipr.Core.CodeModel.Vocabularies.Capabilities
+{
+    /*      <ComplexType Name="ExpandRestrictionsType">
+        <Property Name="Expandable" Type="Edm.Boolean" DefaultValue="true">
+          <Annotation Term="Core.Description" String="$expand is supported" />
+        </Property>
+        <Property Name="NonExpandableProperties" Type="Collection(Edm.NavigationPropertyPath)">
+          <Annotation Term="Core.Description" String="These properties cannot be used in $expand expressions" />
+        </Property>
+      </ComplexType>
+*/
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExpandRestrictionsType
+    {
+        /// <summary>
+        /// $expand is supported
+        /// </summary>
+        public bool Expandable { get; set; }
+
+        /// <summary>
+        /// These properties cannot be used in $expand expressions
+        /// </summary>
+        public List<string> NonExpandableProperties { get; set; }
+
+        public ExpandRestrictionsType()
+        {
+            Expandable = true;
+            NonExpandableProperties = new List<string>();
+        }
+    }
+}
