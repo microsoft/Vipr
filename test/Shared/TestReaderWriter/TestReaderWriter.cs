06fafdfbd5a3e3fdc5d45b2a1d4c0f3ac6055215
piotrpMSFT <piotrp@microsoft.com>
2015-02-20 17:15:14 -0800

    Fix location of TestReaderWriter and DoubleReader helper projects

---
+++ test/Shared/TestReaderWriter/TestReaderWriter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Vipr.Core;
+using Vipr.Core.CodeModel;
+
+namespace TestReaderWriter
+{
+    public class TestReaderWriter:IOdcmReader, IOdcmWriter
+    {
+        public OdcmModel GenerateOdcmModel(TextFileCollection serviceMetadata)
+        {
+            return new OdcmModel(new TextFileCollection {new TextFile("$metadata", "")});
+        }
+
+        public TextFileCollection GenerateProxy(OdcmModel model)
+        {
+            return new TextFileCollection();
+        }
+    }
+}
