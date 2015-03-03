c58798db92bbd4671efb68432e2c5c3bd72d881e
piotrpMSFT <piotrp@microsoft.com>
2015-03-02 17:22:30 -0800

    Remove duplicate implementation of BeDecoratedWith from PropertyInfoAssertionExtensions

--- test/CSharpWriterUnitTests/PropertyInfoAssertionsExtensions.cs
+++ test/CSharpWriterUnitTests/PropertyInfoAssertionsExtensions.cs
@@ -14,27 +14,6 @@ namespace CSharpWriterUnitTests
     public static class PropertyInfoAssertionsExtensions
     {
         /// <summary>
-        /// Asserts that the current <see cref="T:System.Reflection.PropertyInfo"/> is decorated with an attribute of type <typeparamref name="TAttribute"/>.
-        /// 
-        /// </summary>
-        /// <param name="propertyInfoAssertions">The PropertyInfoAssertions to act upon.</param>
-        /// <param name="because">A formatted phrase as is supported by <see cref="M:System.String.Format(System.String,System.Object[])"/> explaining why the assertion
-        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.</param>
-        /// <param name="reasonArgs">Zero or more objects to format using the placeholders in <see cref="!:because"/>.</param>
-        public static AndConstraint<PropertyInfoAssertions> BeDecoratedWith<TAttribute>(
-            this PropertyInfoAssertions propertyInfoAssertions,
-            string because = "", params object[] reasonArgs) where TAttribute : Attribute
-        {
-            Execute.Assertion.ForCondition(propertyInfoAssertions.Subject.GetCustomAttributes(typeof (TAttribute)).Any())
-                .BecauseOf(because, reasonArgs)
-                .FailWith(
-                    "Expected type {0} to be decorated with {1}, but no matching attribute was found.",
-                    (object) propertyInfoAssertions.Subject, (object) typeof (TAttribute));
-
-            return new AndConstraint<PropertyInfoAssertions>(propertyInfoAssertions);
-        }
-
-        /// <summary>
         /// Asserts that the current <see cref="T:System.Reflection.PropertyInfo"/> is decorated with an attribute of type <typeparamref name="TAttribute"/>
         ///             that matches the specified <paramref name="isMatchingAttributePredicate"/>.
         /// 
@@ -46,7 +25,7 @@ namespace CSharpWriterUnitTests
         /// <param name="reasonArgs">Zero or more objects to format using the placeholders in <see cref="!:because"/>.</param>
         public static AndConstraint<PropertyInfoAssertions> BeDecoratedWith<TAttribute>(this PropertyInfoAssertions propertyInfoAssertions, Expression<Func<TAttribute, bool>> isMatchingAttributePredicate, string because = "", params object[] reasonArgs) where TAttribute : Attribute
         {
-            BeDecoratedWith<TAttribute>(propertyInfoAssertions, because, reasonArgs);
+            propertyInfoAssertions.BeDecoratedWith<TAttribute>(because, reasonArgs);
             
             Execute.Assertion.ForCondition(propertyInfoAssertions.Subject.GetCustomAttributes(typeof (TAttribute))
                 .Count(a => isMatchingAttributePredicate.Compile().Invoke((TAttribute) a)) > 0)
