2517c027c890baa00503e92ecca7c18abd395e2f
Sridhar Periyasamy <sridhper@microsoft.com>
2015-03-30 17:34:24 -0700

    Removal of Capabilities Annotations parsing logic from the OdcmModel
    OdcmModel is designed to be protocol agnostic. Earlier OData concepts like Capabilities annotations parsing were wrongly introduced into the OdcmModel in the spirit of code simplification. But it violated a core principle of the OdcmModel that it should be protocol agnostic. The fix is to abstract away OData Capabilities annotations parsing into the ODataReader.
    
    Fix #60

---
+++ src/Readers/ODataReader.v4/Capabilities/BooleanCapabilityAnnotationParser.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Annotations;
+using Vipr.Core.CodeModel;
+using Microsoft.OData.Edm.Expressions;
+using Vipr.Core;
+using Vipr.Core.CodeModel.Vocabularies.Capabilities;
+
+namespace ODataReader.v4.Capabilities
+{
+    public class BooleanCapabilityAnnotationParser : CapabilityAnnotationParser
+    {
+        private static List<OdcmBooleanCapability> _supportedOdcmCapabilities;
+
+        public BooleanCapabilityAnnotationParser()
+        {
+            _supportedOdcmCapabilities = new List<OdcmBooleanCapability>();
+            var viprCoreName = Assembly.GetExecutingAssembly().GetReferencedAssemblies().First(a => a.Name == "Vipr.Core");
+            var capabilityTypes = Assembly.Load(viprCoreName).GetTypes().Where(t => t.IsSubclassOf(typeof(OdcmBooleanCapability)) && !t.IsAbstract);
+
+            foreach (var capabilityType in capabilityTypes)
+            {
+                var capability = (OdcmBooleanCapability)Activator.CreateInstance(capabilityType);
+                _supportedOdcmCapabilities.Add(capability);
+            }
+        }
+
+        public override void  ParseCapabilityAnnotationForEntitySet(OdcmProperty odcmEntitySet,
+            IEdmValueAnnotation annotation, ODataCapabilitiesReader.PropertyCapabilitiesCache propertyCache)
+        {
+            if (!_supportedOdcmCapabilities.Any(c => c.TermName == annotation.Term.FullName()))
+            {
+                return;
+            }
+
+            var recordExpression = (IEdmRecordExpression)annotation.Value;
+
+            // First get the boolean value for this annotation on an EntitySet.
+            // Then create a corresponding OdcmCapability and add it to the OdcmProperty of the EntitySet
+            var boolVal = GetBooleanValue(recordExpression);
+            var odcmCapability = GetBooleanCapabiltity(boolVal, annotation.Term.FullName());
+            propertyCache.AddCapabilityToProperty(odcmEntitySet, odcmCapability);
+
+            // Get the list of annotated navigation properties
+            // Then resolve these navigation properties to OdcmProperties
+            // Lastly create corresponding OdcmCapabilities and add them to OdcmProperties (of the resolved navigation Properties)
+            List<OdcmProperty> navigationProperties = GetNavigationProperties(recordExpression,
+                odcmEntitySet.Type as OdcmClass);
+
+            foreach (var navigationProperty in navigationProperties)
+            {
+                odcmCapability = GetBooleanCapabiltity(false, annotation.Term.FullName());
+                propertyCache.AddCapabilityToProperty(navigationProperty, odcmCapability);
+            }
+        }
+
+        private bool GetBooleanValue(IEdmRecordExpression recordExpression)
+        {
+            var booleanExpression = (IEdmBooleanConstantExpression)recordExpression.Properties.Single(p => p.Value is IEdmBooleanConstantExpression).Value;
+            return booleanExpression.Value;
+        }
+
+        private List<OdcmProperty> GetNavigationProperties(IEdmRecordExpression recordExpression, OdcmClass @class)
+        {
+            var properties = new List<OdcmProperty>();
+            var collectionExpression = (IEdmCollectionExpression)recordExpression.Properties.Single(p => p.Value is IEdmCollectionExpression).Value;
+
+            if (collectionExpression == null)
+            {
+                return properties;
+            }
+
+            foreach (IEdmPathExpression pathExpression in collectionExpression.Elements)
+            {
+                var pathBuilder = new StringBuilder();
+                foreach (var path in pathExpression.Path)
+                {
+                    pathBuilder.AppendFormat("{0}.", path);
+                }
+
+                pathBuilder.Remove(pathBuilder.Length - 1, 1);
+
+                OdcmProperty navProperty;
+                if (!@class.TryFindProperty(pathBuilder.ToString(), out navProperty))
+                {
+                    throw new InvalidOperationException(
+                        string.Format(
+                            "Unable to find property {0} in class {1}. This can be caused by malformed Capability Annotation on an EntitySet",
+                            pathBuilder.ToString(), @class.FullName));
+                }
+                properties.Add(navProperty);
+            }
+
+            return properties;
+        }
+
+        private OdcmCapability GetBooleanCapabiltity(bool boolVal, string annotationTermName)
+        {
+            Type capabilityType = _supportedOdcmCapabilities.Single(c => c.TermName == annotationTermName).GetType();
+            var capability = (OdcmBooleanCapability)Activator.CreateInstance(capabilityType);
+            capability.Value = boolVal;
+            return capability;
+        }
+    }
+}
---
+++ src/Readers/ODataReader.v4/Capabilities/ODataCapabilitiesReader.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Vipr.Core.CodeModel;
+
+namespace ODataReader.v4.Capabilities
+{
+    public partial class ODataCapabilitiesReader
+    {
+        private static List<CapabilityAnnotationParser> _defaultCapabilityAnnotationParsers;
+
+        /// <summary>
+        /// Default list of Capability Annotation parsers
+        /// </summary>
+        public static List<CapabilityAnnotationParser> DefaultCapabilityAnnotationParsers
+        {
+            get
+            {
+                if (_defaultCapabilityAnnotationParsers == null)
+                {
+                    _defaultCapabilityAnnotationParsers = new List<CapabilityAnnotationParser>();
+                    IEnumerable<Type> parserTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(CapabilityAnnotationParser)) && !t.IsAbstract);
+
+                    foreach (var parserType in parserTypes)
+                    {
+                        var parser = (CapabilityAnnotationParser)Activator.CreateInstance(parserType);
+                        _defaultCapabilityAnnotationParsers.Add(parser);
+                    }
+                }
+
+                return _defaultCapabilityAnnotationParsers;
+            }
+        }
+
+        public static void SetCapabilitiesForEntityContainer(OdcmServiceClass odcmServiceClass, IEdmEntityContainer edmEntityContainer, IEdmModel serviceModel)
+        {
+            //TODO: Add Capability Annotation support for EntityContainers
+        }
+
+        /// <summary>
+        /// Sets the OdcmCapabilities for the given annotated EntitySet and also for the annotated navigation properties.
+        /// </summary>
+        public static void SetCapabilitiesForEntitySet(OdcmProperty odcmEntitySet, IEdmEntitySet edmEntitySet, IEdmModel serviceModel)
+        {
+            if (odcmEntitySet == null) throw new ArgumentNullException("odcmEntitySet");
+            if (edmEntitySet == null) throw new ArgumentNullException("edmEntitySet");
+            if (serviceModel == null) throw new ArgumentNullException("serviceModel");
+
+            var annotations = serviceModel.FindVocabularyAnnotations(edmEntitySet);
+            PropertyCapabilitiesCache propertyCache = new PropertyCapabilitiesCache();
+
+            foreach (var annotation in annotations)
+            {
+                foreach (var annotationParser in DefaultCapabilityAnnotationParsers)
+                {
+                    annotationParser.ParseCapabilityAnnotationForEntitySet(odcmEntitySet, (IEdmValueAnnotation)annotation, propertyCache);
+                }
+            }
+
+            propertyCache.EnsureProjectionsForProperties();
+        }
+    }
+}
