e7b0476a87ef3c6ac8ccd452f1a5483550e46289
Sridhar Periyasamy <sridhper@microsoft.com>
2015-04-07 15:02:46 -0700

    Create unit tests project for Microsoft.OData.ProxyExtensions.Lite.
    It is forked from the current ProxyExtensionsUnitTests project and new tests/changes will be added on top of it.

---
+++ test/ProxyExtensionsLiteUnitTests/Given_A_Query_For_EntitySet.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Its.Recipes;
+using Microsoft.OData.ProxyExtensions;
+using ODataV4TestService.SelfHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.OData.Client;
+using Microsoft.Spatial;
+using Xunit;
+
+namespace ProxyExtensionsUnitTests
+{
+    public class Given_A_Query_For_EntitySet : ProxyExtensionsUnitTestsBase
+    {
+        [Fact]
+        public void When_Querying_For_Existing_EntitySet_Then_Return_The_EntitySet()
+        {
+            using (var scenario =
+                    new ODataScenario()
+                        .WithProducts(Any.Products())
+                        .Start())
+            {
+                var context = GetDataServiceContext(scenario.GetBaseAddress());
+                var dQuery = context.CreateQuery<Product>("/" + "Products");
+
+                //query for all the products
+                var products = context.ExecuteAsync<Product, IProduct>(dQuery).Result;
+                products.CurrentPage.Count.Should().Be(5);
+                (products.CurrentPage[2] as Product).Id.Should().Be(3);
+            }
+        }
+
+        [Fact]
+        public void When_Querying_For_Existing_EntitySet_Using_EntityType_Interface_Then_Return_The_EntitySet()
+        {
+            using (var scenario =
+                    new ODataScenario()
+                        .WithProducts(Any.Products())
+                        .Start())
+            {
+                var context = GetDataServiceContext(scenario.GetBaseAddress());
+                var dQuery = context.CreateQuery<Product>("/" + "Products");
+                IReadOnlyQueryableSet<IProduct> readOnlySet = new ReadOnlyQueryableSet<IProduct>(dQuery, context);
+
+                var products = readOnlySet.ExecuteAsync().Result;
+                products.CurrentPage.Count.Should().Be(5);
+                (products.CurrentPage[2] as Product).Id.Should().Be(3);
+            }
+        }
+
+        [Fact]
+        public void When_Querying_For_NonExistent_EntitySet_Then_Throw_DataServiceQueryException_With_NotFound_Status_Code()
+        {
+            using (var scenario =
+                    new ODataScenario()
+                        .WithProducts(Any.Products())
+                        .Start())
+            {
+                var context = GetDataServiceContext(scenario.GetBaseAddress());
+
+                var action = new Action(() =>
+                {
+                    var dQuery = context.CreateQuery<Product>("/" + "RandomEntitySet");
+                    var products = context.ExecuteAsync<Product, IProduct>(dQuery).Result;
+                });
+
+                //check for AggregateException with DataServiceQueryException as InnerException.
+                //Also retrieve the DataServiceQueryException InnerException.
+                DataServiceQueryException innerException = action.ShouldThrow<AggregateException>()
+                    .WithInnerException<DataServiceQueryException>()
+                    .And.InnerException as DataServiceQueryException;
+
+                //Check for 'Not Found' code.
+                innerException.Response.StatusCode.Should().Be(404);
+            }
+        }
+    }
+}
---
+++ test/ProxyExtensionsLiteUnitTests/Given_An_EntityType_Created_On_Client.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Its.Recipes;
+using Microsoft.OData.ProxyExtensions;
+using ODataV4TestService.SelfHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.OData.Client;
+using Microsoft.Spatial;
+using Xunit;
+
+namespace ProxyExtensionsUnitTests
+{
+    public class Given_An_EntityType_Created_On_Client : ProxyExtensionsUnitTestsBase
+    {
+        [Fact]
+        public void When_Adding_EntityType_With_All_Properties_Then_It_Must_Be_Added_To_Server_With_All_Properties()
+        {
+            using (var scenario =
+                    new ODataScenario()
+                        .WithProducts(Any.Products())
+                        .Start())
+            {
+                var context = GetDataServiceContext(scenario.GetBaseAddress());
+                context.MergeOption = MergeOption.OverwriteChanges;
+                var dQuery = context.CreateQuery<Product>("/" + "Products");
+
+                var products = context.ExecuteAsync<Product, IProduct>(dQuery).Result;
+                products.CurrentPage.Count.Should().Be(5);
+
+                var newProduct = new Product();
+                newProduct.Id = 6;
+                newProduct.Name = Any.CompanyName();
+                newProduct.Price = Any.Decimal();
+                newProduct.Category = Any.AlphanumericString();
+                newProduct.OnPropertyChanged("Id");
+                newProduct.OnPropertyChanged("Name");
+                newProduct.OnPropertyChanged("Price");
+                newProduct.OnPropertyChanged("Category");
+
+                context.AddObject("Products", newProduct);
+                context.SaveChangesAsync().Wait();
+
+                var updatedProducts = context.ExecuteAsync<Product, IProduct>(dQuery).Result;
+                updatedProducts.CurrentPage.Count.Should().Be(6);
+                updatedProducts.CurrentPage[5].Should().BeSameAs(newProduct);
+            }
+        }
+
+
+        [Fact]
+        public void When_Adding_EntityType_With_Partial_Properties_Then_It_Must_Be_Added_To_Server_With_Partial_Properties()
+        {
+            using (var scenario =
+                    new ODataScenario()
+                        .WithProducts(Any.Products())
+                        .Start())
+            {
+                var context = GetDataServiceContext(scenario.GetBaseAddress());
+                var dQuery = context.CreateQuery<Product>("/" + "Products");
+
+                var products = context.ExecuteAsync<Product, IProduct>(dQuery).Result;
+                products.CurrentPage.Count.Should().Be(5);
+
+                var newProduct = new Product();
+                string newName = Any.CompanyName();
+                newProduct.Id = 6;
+                newProduct.Name = newName;
+                newProduct.Price = Any.Decimal();
+                newProduct.Category = Any.AlphanumericString();
+
+                // calling 'OnPropertyChanged' only for 'Id' and 'Name' properties
+                newProduct.OnPropertyChanged("Id");
+                newProduct.OnPropertyChanged("Name");
+
+                context.AddObject("Products", newProduct);
+                context.SaveChangesAsync().Wait();
+
+                var updatedProducts = context.ExecuteAsync<Product, IProduct>(dQuery).Result;
+                updatedProducts.CurrentPage.Count.Should().Be(6);
+
+                // the 'Id' and 'Name' properties must be set
+                updatedProducts.CurrentPage[5].Id.Should().Be(6);
+                updatedProducts.CurrentPage[5].Name.Should().Be(newName);
+
+                // the 'Price' and 'Category' properties must not be set
+                updatedProducts.CurrentPage[5].Price.Should().Be(0);
+                updatedProducts.CurrentPage[5].Category.Should().BeNull();
+            }
+        }
+    }
+}
