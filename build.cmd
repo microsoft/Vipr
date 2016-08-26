563f0aa192628d8c0773c38400b344fdcfa44cd1
Robert Anderson <robert.anderson@microsoft.com>
2016-08-26 11:59:22 -0700

    Updating to only use latest MSBuild.

--- build.cmd
+++ build.cmd
@@ -2,9 +2,7 @@
 setlocal
 
 :: Check prerequisites
-set _msbuildexe="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
-if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+set _msbuildexe="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
 if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
 if not exist %_msbuildexe% echo Error: Could not find MSBuild.exe.  Please see https://github.com/Microsoft/vipr/wiki/Developer%%20Guide for build instructions. && exit /b 2
 
