3a1002b0eb414d1f870251dc7503ca41b7a2dd7e
Caitlin Bales (MSFT) <c.bales@outlook.com>
2017-08-18 15:41:10 -0700

    Switched to async locks with awaits Uses batching to prevent I/O overload. Saves about 1m of processing time

--- src/Core/Vipr/FileWriter.cs
+++ src/Core/Vipr/FileWriter.cs
@@ -1,6 +1,9 @@
+using Nito.AsyncEx;
 using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,15 +13,24 @@ namespace Vipr
 {
     internal static class FileWriter
     {
-        public static async void WriteAsync(IEnumerable<TextFile> textFilesToWrite, string outputDirectoryPath = null)
+        private static ConcurrentDictionary<string, AsyncLock> lockDictionary = new ConcurrentDictionary<string, AsyncLock>();
+
+        public static void WriteAsync(IEnumerable<TextFile> textFilesToWrite, string outputDirectoryPath = null)
         {
             if (!string.IsNullOrWhiteSpace(outputDirectoryPath) && !Directory.Exists(outputDirectoryPath))
                 Directory.CreateDirectory(outputDirectoryPath);
 
-            var fileTasks = new List<Task>();
+            /* Write asynchronously in batches of 50
+            * This prevents I/O from slowing down
+            * from an overload of requests
+            */
+            var batchSize = 50;
+            var batchCount = 0;
+            List<Task> tasks = new List<Task>();
 
             foreach (var file in textFilesToWrite)
             {
+                tasks = new List<Task>();
                 var filePath = file.RelativePath;
 
                 if (!string.IsNullOrWhiteSpace(outputDirectoryPath))
@@ -31,37 +43,38 @@ namespace Vipr
                 if (!Directory.Exists(Path.GetDirectoryName(filePath)))
                     Directory.CreateDirectory(Path.GetDirectoryName(filePath));
 
-                fileTasks.Add(WriteToDisk(filePath, file.Contents));
+                // Create a new async task to write the file
+                tasks.Add(WriteToDisk(filePath, file.Contents));
+                batchCount++;
+
+                // Reached the batch limit. Wait for all tasks to complete
+                if (batchCount > batchSize)
+                {
+                    Task t = Task.WhenAll(tasks);
+                    t.Wait();
+                    batchCount = 0;
+                }
             }
-            await Task.WhenAll(fileTasks);
+
+            // Final task wait
+            if (tasks.Count > 0)
+            {
+                Task t = Task.WhenAll(tasks);
+                t.Wait();
+            }
         }
 
-        /**
-         * Write the file to disk. If the file is locked for editing,
-         * sleep until available
-         */
         public static async Task WriteToDisk(string filePath, string output)
         {
-            for (int tries = 0; tries < 10; tries++)
+            var lockItem = lockDictionary.GetOrAdd(filePath, new AsyncLock());
+
+            using (await lockItem.LockAsync())
             {
-                StreamWriter sw = null;
-                try
+                using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
                 {
-                    using (sw = new StreamWriter(filePath, false, Encoding.UTF8))
-                    {
-                        await sw.WriteAsync(output);
-                        break;
-                    }
-                }
-                // If the file is currently locked for editing, sleep
-                // This shouldn't be hit if the generator is running correctly,
-                // however, files are currently being overwritten several times
-                catch (IOException)
-                {
-                    Thread.Sleep(5);
+                    await sw.WriteAsync(output);
                 }
             }
-
         }
     }
 }
