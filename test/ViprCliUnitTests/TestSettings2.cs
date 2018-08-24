d79f2dc43b4c7160cdac394d403bab1ca7552a17
Michael Mainer <mmainer@microsoft.com>
2018-08-23 18:21:05 -0700

    Removed unused legacy projects.

--- test/ViprCliUnitTests/TestSettings2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ViprUnitTests
-{
-    // Its.Configuration can cache settings based on Type
-    // To avoid test issues, creating a second type to
-    // bust the cache
-    public class TestSettings2 : TestSettings
-    {
-    }
-}
